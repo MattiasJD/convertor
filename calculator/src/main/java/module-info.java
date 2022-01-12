3c00946356fe2c7bef7865ec5f968649b8795f54
MattiasJD <90755599+MattiasJD@users.noreply.github.com>
2022-01-12 14:55:47 +0100

    Delete calculator directory

--- calculator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cz.spsmb.javafx.calculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens cz.spsmb.javafx.calculator to javafx.fxml;
-    exports cz.spsmb.javafx.calculator;
-}
